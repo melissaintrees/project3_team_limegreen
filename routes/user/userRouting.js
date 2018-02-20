1870f8fc12ca1575d147536b7c8a70a9cd403558
dnovakovic15 <dnovakovic21@yahoo.com>
2018-02-20 10:52:01 -0600

    Log In functionality added.

---
+++ routes/user/userRouting.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const jwt = require('jsonwebtoken');
+const users = require("../../controllers/logInController.js");
+const newUser = require("../../controllers/registerController.js");
+
+// Matches with "/api/questions"
+router.route("/")
+  .get((req, res) => res.send('Hello World'));
+
+router.post("/login", function(req, res) {
+    console.log(req.body.username);
+    users(req.body.username, req.body.password, cb, res);
+});
+
+router.get("/signup/:username/:password", function(req, res) {
+    newUser(req.body.username, req.body.password, cb, res);
+  });
+
+router.get("/userAccount/:token", function(req, res){
+  verify(req.params.token);
+});
+
+function cb(result, res, userName){
+    let token = false;
+
+    if(result){
+      let token = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + (60 * 60),
+        data: userName,
+      }, 'WeAreSparTans');
+      res.send(token);
+    }
+    else{
+      console.log('Log in Failed')
+      res.send(token);
+    }
+}
+
+function verify(token){
+  let decoded = jwt.verify(token, 'WeAreSparTans');
+  console.log(decoded);
+}
+
+
+module.exports = router;
