1cabc676c49e9ef798340fa215d3008fa48ecc38
dnovakovic15 <dnovakovic21@yahoo.com>
2018-02-22 19:38:49 -0600

    Coors error fix

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -33,8 +33,6 @@ module.exports = {
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    res.send('true');    
     
     let id  = req.params.id;
 
