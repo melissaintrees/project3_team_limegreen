b980257756e47f2439956ae7046071924e756cf4
dnovakovic15 <dnovakovic21@yahoo.com>
2018-02-27 20:52:48 -0600

    coors

--- routes/user/userRouting.js
+++ routes/user/userRouting.js
@@ -8,6 +8,17 @@ router.route("/")
   .get((req, res) => res.send('Hello World'));
 
 router.post("/login", function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
     console.log(req.body.username);
     users(req.body.username, req.body.password, cb, res);
 });
