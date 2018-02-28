10683c289a43dd72423c4b3d593c3820e014422b
dnovakovic15 <dnovakovic21@yahoo.com>
2018-02-27 20:50:05 -0600

    Coors fix

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -4,6 +4,18 @@ let bcrypt = require('bcrypt-nodejs');
 
 function addUser(userName, password, cb, res){
     console.log(userName);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
     bcrypt.hash(password, 5, function( err, bcryptedPassword) {
         let newPassword = bcryptedPassword;
         console.log(newPassword);
