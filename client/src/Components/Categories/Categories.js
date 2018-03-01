824a5826d5e4e5683091e48e049732606b60ab50
melissaintrees <melissaintrees@gmail.com>
2018-03-01 13:22:37 -0600

    fix categories pages

--- client/src/Components/Categories/Categories.js
+++ client/src/Components/Categories/Categories.js
@@ -5,17 +5,17 @@ const Categories = props => (
     <div>
         <div className="html">
             <button type="button" className="btn btn-html btn-lg btn-block" {...props}>
-            <img className="img-div" alt="cute lion cartoon" src={require('../../static_code_aquarium/images/penguin.png')} />HTML
+            <img className="img-div-categ" alt="cute lion cartoon" src={require('../../static_code_aquarium/images/penguin.png')} /> <p class="categ-text">HTML PENGUINS</p>
             </button>
         </div>
         <div className="css">
             <button type="button" className="btn btn-css btn-lg btn-block" {...props}>
-            <img className="img-div" alt="cute lion cartoon" src={require('../../static_code_aquarium/images/zebra.png')} />CSS
+                <img className="img-div-categ" alt="cute lion cartoon" src={require('../../static_code_aquarium/images/zebra.png')} /> <p class="categ-text">CSS ZEBRAS</p>
             </button>
         </div>
         <div className="loops">
             <button type="button" className="btn btn-loops btn-lg btn-block" {...props}>
-            <img className="img-div" alt="cute lion cartoon" src={require('../../static_code_aquarium/images/squirrely.png')} />LOOPS
+                <img className="img-div-categ" alt="cute lion cartoon" src={require('../../static_code_aquarium/images/squirrely.png')} /> <p class="categ-text">LOOPY SQUIRRELS</p>
             </button>
         </div>
     </div>
