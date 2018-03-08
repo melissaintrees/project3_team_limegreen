b9a0ac7d31d44bc33c6ac7ed6f4b63a6e35c4e8c
dnovakovic15 <dnovakovic21@yahoo.com>
2018-03-08 15:46:17 -0600

    Added Server routing to server

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const userRoutes = require("./user");
 const express = require("express");
+let con = require('../config/connection.js');
 
 // router.use("/", express.static(path.join(__dirname, 'client/build')));
 
@@ -11,6 +12,17 @@ router.use("/api", apiRoutes);
 
 //Register and Sign-In Routes
 router.use("/users", userRoutes)
+
+//Scoring
+router.post("/score", function(req, res) {
+    let sql = `INSERT INTO users (score) VALUES ("${req.body.score}") WHERE name=${req.body.username};`
+    
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        cb(result);
+    });
+  });
 
 // If no API routes are hit, send the React app
 // router.use("*", function(req, res) {
